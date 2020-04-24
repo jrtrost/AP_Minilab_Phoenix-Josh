5ce6294e9db3700cc4b984ccd7434f4a0d26c671
lucyygao <42822456+lucyygao@users.noreply.github.com>
2020-04-24 15:41:44 -0700

    ui test

---
+++ src/UITest.java
@@ -0,0 +1,63 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+
+public class UITest extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UITest frame = new UITest();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public UITest() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1162, 806);
+		
+		// JLabel with image 
+		JLabel pic = new JLabel("");        
+		pic.setBackground(Color.WHITE);
+        java.awt.Image image = new ImageIcon("img/APCS1.PNG").getImage();
+        pic.setIcon(new ImageIcon(image));
+        pic.setBounds(377, 111, 128, 128);
+        
+        // Content Panel to add Label and Image
+        //https://docs.oracle.com/javase/tutorial/uiswing/components/rootpane.html
+        Container content = getContentPane();
+        content.setBackground(new Color(100,100,255));
+        content.add(pic);
+        
+ 
+	}
+	
+}
